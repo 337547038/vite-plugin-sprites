e1d93b0d3dbabd3301a4984c41c15189fd327a1f
337547038 <337547038@qq.com>
2023-06-25 18:00:39 +0800

    修复icon目录不存在时报错

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-//import sprites from 'vite-plugin-sprites'
-import sprites from './packages/src'
+import sprites from 'vite-plugin-sprites'
+//import sprites from './packages/src'
 
 // https://vitejs.dev/config/
 export default defineConfig({
